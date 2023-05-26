a7e0198227103cd3941248e09d232cf50a0d148a
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-05-26 09:06:06 -0300

    Diretiva ngStyle

--- aulas-angular/capitulo05/projeto-diretivas/src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ aulas-angular/capitulo05/projeto-diretivas/src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -8,12 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class DiretivasAtributosComponent implements OnInit{
 
   public value: boolean = true;
+  public heightPx: string = "20px";
+  public backgroundColor: string = "gold";
 
   constructor(){}
 
   ngOnInit(): void {
     setInterval(() => {
       this.value = this.value === true ? false : true;
+
+      if(this.heightPx === "20px"){
+        this.heightPx = "50px";
+        this.backgroundColor = "blue";
+      }
+      else{
+        this.heightPx = "20px";
+        this.backgroundColor = "gold";
+      }
     }, 2000);
   }
 }
