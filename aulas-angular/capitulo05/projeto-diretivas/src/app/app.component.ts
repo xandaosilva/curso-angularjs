a7e6cf49c3d6d825062057900bc019f09b60c96c
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-05-26 09:46:54 -0300

    Diretiva ng-content

--- aulas-angular/capitulo05/projeto-diretivas/src/app/app.component.ts
+++ aulas-angular/capitulo05/projeto-diretivas/src/app/app.component.ts
@@ -5,7 +5,11 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <app-title></app-title>
     <app-diretivas-estruturais></app-diretivas-estruturais>
-    <app-diretivas-atributos></app-diretivas-atributos>
+    <app-diretivas-atributos>
+      <h1>Aulas de diretivas - Atributo</h1>
+      <h3>Final da aula de diretivas</h3>
+      <hr>
+    </app-diretivas-atributos>
     <router-outlet></router-outlet>
   `
 })
