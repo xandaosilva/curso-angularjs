11cbe9c650d3f981b33f8c0b86726175fcb117c0
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-06-01 19:30:49 -0300

    Componente todoList

--- aulas-angular/capitulo08/projeto-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ aulas-angular/capitulo08/projeto-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+
+  public taskList: Array<TaskList> = [
+    { task: "Primeira tarefa", checked: true },
+    { task: "Segunda tarefa", checked: false },
+    { task: "Terceira tarefa", checked: true },
+    { task: "Quarta tarefa", checked: true },
+    { task: "Quinta tarefa", checked: false },
+  ];
 
   constructor(){}
 
