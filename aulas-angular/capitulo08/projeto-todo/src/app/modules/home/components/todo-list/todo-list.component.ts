b923976fdbc3c52e6de2203fbcb83a8fb11ea0b6
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-06-05 16:52:43 -0300

    Validando lista para deleção de tarefas, aplicação de estilos em tarefas realizadas e ordenação das tarefas

--- aulas-angular/capitulo08/projeto-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ aulas-angular/capitulo08/projeto-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
 import { TaskList } from '../../model/task-list';
 
 @Component({
@@ -6,7 +6,7 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
 
   public taskList: Array<TaskList> = [
     { task: "Primeira tarefa", checked: true },
@@ -18,7 +18,8 @@ export class TodoListComponent implements OnInit {
 
   constructor(){}
 
-  ngOnInit(): void {
+  ngDoCheck(): void {
+    this.taskList.sort( (first, last) => Number(first.checked) - Number(last.checked));
   }
 
   public deleteItemTaskList(event: number){
@@ -35,5 +36,15 @@ export class TodoListComponent implements OnInit {
 
   public setEmitTaskList(event: string){
     this.taskList.push({ task: event, checked: false });
+  }
+
+  public validationInput(event: string, index: number){
+    if(!event.length){
+      const confirm = window.confirm("Task está vazia, deseja deletar ?");
+      
+      if(confirm){
+        this.deleteItemTaskList(index);
+      }
+    }
   }
 }
