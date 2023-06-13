2e1627f34caf3b7e62286f8a24b9607b10dd0453
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-06-12 22:57:06 -0300

    Navegação entre páginas com router link

--- aulas-angular/capitulo12/projeto-roteamento/src/app/app.module.ts
+++ aulas-angular/capitulo12/projeto-roteamento/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
