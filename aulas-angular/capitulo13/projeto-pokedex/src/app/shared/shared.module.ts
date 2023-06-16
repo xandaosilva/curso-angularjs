e47f88293b307ff824b0ff42ab8a563070054169
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-06-16 00:30:32 -0300

    Router link para o componente Details

--- aulas-angular/capitulo13/projeto-pokedex/src/app/shared/shared.module.ts
+++ aulas-angular/capitulo13/projeto-pokedex/src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
 import { PokeListComponent } from './poke-list/poke-list.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -13,7 +14,8 @@ import { PokeListComponent } from './poke-list/poke-list.component';
     PokeListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     PokeHeaderComponent,
