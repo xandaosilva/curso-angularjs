3a7dd7eaef7ba69ffad7d054cf38846a1e5de4ff
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-06-20 01:11:13 -0300

    Criando o projeto Guards e utilizando a diretiva CanActivate

---
+++ aulas-angular/capitulo15/projeto-guards/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './shared/home/home.component';
+import { AccountComponent } from './shared/account/account.component';
+import { CanActiveGuard } from './shared/guards/can-active.guard';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { 
+    path: 'account',
+    component: AccountComponent,
+    canActivate: [CanActiveGuard] 
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ aulas-angular/capitulo15/projeto-guards/src/app/shared/guards/can-active.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CanActiveGuard implements CanActivate {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if(route.queryParams['account'] === 'admin' && route.queryParams['password'] === '123'){
+      console.log(route);
+      console.log(state);
+      return true;
+    }
+    return false;
+  }
+  
+}
