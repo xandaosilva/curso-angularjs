69297f479a4fdddc1724fd923393a4f08760d3b1
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-06-21 05:31:16 -0300

    Standalone Components

---
+++ aulas-angular/capitulo18/projeto-atualizacoes/src/main.ts
@@ -0,0 +1,10 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { importProvidersFrom } from '@angular/core';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(AppRoutingModule)
+  ]
+}).catch(err => console.error(err));
