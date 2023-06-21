3aa88eb3c94b732905468ba19cec52bae82f82a6
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-06-21 04:46:24 -0300

    Criando componentes Home e About, criando as rotas e finalizando o projeot Theme

--- aulas-angular/capitulo17/projeto-theme/src/app/app.module.ts
+++ aulas-angular/capitulo17/projeto-theme/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/components/header/header.component';
+import { HomeComponent } from './core/pages/components/home/home.component';
+import { AboutComponent } from './core/pages/components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
