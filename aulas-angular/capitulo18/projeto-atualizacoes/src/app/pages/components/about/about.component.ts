ef231f9d3aaa3ae087bdd50cb1035f73b2eb430b
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-06-21 05:39:14 -0300

    Reactive forms tipados

--- aulas-angular/capitulo18/projeto-atualizacoes/src/app/pages/components/about/about.component.ts
+++ aulas-angular/capitulo18/projeto-atualizacoes/src/app/pages/components/about/about.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
+  public form = this.formBuilder.group(
+    {
+      email: ['admin@gmail.com'],
+      password: ['1234567890']
+    }
+  );
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    console.log(this.form.value.email);
+    console.log(this.form.value.password);
+  }
 }
