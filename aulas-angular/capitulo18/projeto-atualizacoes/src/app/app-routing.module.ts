03039b8a4a6afd1d9fa0a5ab8e678fd79a1e6fb8
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-06-21 05:41:21 -0300

    Acessibilidade simplificada do título da página

--- aulas-angular/capitulo18/projeto-atualizacoes/src/app/app-routing.module.ts
+++ aulas-angular/capitulo18/projeto-atualizacoes/src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { HomeComponent } from './pages/components/home/home.component';
 import { AboutComponent } from './pages/components/about/about.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent }
+  { path: '', component: HomeComponent, title: 'Home' },
+  { path: 'about', component: AboutComponent, title: 'About' }
 ];
 
 @NgModule({
