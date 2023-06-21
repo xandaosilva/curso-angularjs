91bfe1071b1720c88a3af5f088801a62b76244cd
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-06-21 03:38:35 -0300

    Auth guard com angular-jwt

--- aulas-angular/capitulo16/projeto-auth-front/src/app/app-routing.module.ts
+++ aulas-angular/capitulo16/projeto-auth-front/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guard/auth.guard';
 
 const routes: Routes = [
   {
@@ -8,7 +9,8 @@ const routes: Routes = [
   },
   {
     path: 'admin',
-    loadChildren: () => import('./components/admin/admin.module').then(m => m.AdminModule)
+    loadChildren: () => import('./components/admin/admin.module').then(m => m.AdminModule),
+    canActivateChild: [AuthGuard]
   }
 ];
 
--- aulas-angular/capitulo16/projeto-auth-front/src/app/core/services/auth.service.ts
+++ aulas-angular/capitulo16/projeto-auth-front/src/app/core/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Observable, catchError, map, throwError } from 'rxjs';
 
 @Injectable({
@@ -31,5 +32,16 @@ export class AuthService {
   public signOut(){
     localStorage.removeItem('access_token');
     return this.router.navigate(['']);
+  }
+
+  public isAuthenticated(): boolean{
+    const token = localStorage.getItem('access_token');
+
+    if(!token){
+      return false;
+    }
+
+    const jwtHelper = new JwtHelperService()
+    return !jwtHelper.isTokenExpired(token);
   }
 }
