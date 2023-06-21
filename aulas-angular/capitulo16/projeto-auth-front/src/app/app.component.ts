d4f5e3fd0ff5c6bc99cb27d0ba226d59cf84a4ca
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-06-20 21:57:59 -0300

    Estilos SCSS do projeto Auth front

--- aulas-angular/capitulo16/projeto-auth-front/src/app/app.component.ts
+++ aulas-angular/capitulo16/projeto-auth-front/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <a [routerLink]="['admin']">Admin</a>
     <router-outlet></router-outlet>
   `
 })
