c4d674b5d24ae89e3341dbc4f231034d942eebe9
Alexandre Rogério <xandaosilva93@hotmail.com>
2023-06-26 01:21:46 -0300

    Seleção de elementos DOM com JavaScript

---
+++ aulas-javascript/capitulo28/aula01/assets/js/scripts.js
@@ -0,0 +1,17 @@
+const title = document.querySelector('h1');
+const classParagraph = document.querySelector('.paragraph');
+const strongParagraph = document.querySelector('.paragraph strong');
+const idParagraph = document.querySelector('#paragraph');
+const allParagraphs = document.querySelectorAll('p');
+const idP = document.getElementById('paragraph');
+const classNameP = document.getElementsByClassName('paragraph');
+const tagName = document.getElementsByTagName('p');
+
+console.log(title);
+console.log(classParagraph);
+console.log(strongParagraph);
+console.log(idParagraph);
+console.log(allParagraphs);
+console.log(idP);
+console.log(classNameP);
+console.log(tagName);
